92c01c5baa3016e6d3a3ca5ed6d03957506cc70d
Priyanshav <priyanshavkmr6@gmail.com>
2023-11-30 12:11:00 +0530

    updated

--- while.py
+++ while.py
@@ -1,6 +1,6 @@
-i = 0
-while(i<=3):
-    print("Python")
-    i+=1
+i = 0 #loop variable
+while(i<=3): #Condition
+    print("Python") #Statements
+    i+=1 #Updation statement
     
 print("end loop")
