28777b6f8e049647b04c1c8266082443199a8c68
Priyanshav <priyanshavkmr6@gmail.com>
2023-12-06 14:26:30 +0530

    inputing tuple and fibonacci

---
+++ fibonaccinumber.py
@@ -0,0 +1,11 @@
+def fibonacci(num):
+    if num==0:
+        return 0
+    elif num==1:
+        return 1
+    else:
+        return fibonacci(num-2)+fibonacci(num-1)
+    
+n = int(input("Enter a number: "))
+for i in range(0,n):
+    print(fibonacci(i))
---
+++ tupleandelement.py
@@ -0,0 +1,4 @@
+t = tuple(map(int,input("Enter elements: ").split()))
+ele = int(input("Enter element: "))
+
+print("Is element present there in tuple: ", ele in t)
