9493e55e9ab94e494674a502cdb2a76ea9835ddc
Priyanshav <priyanshavkmr6@gmail.com>
2023-12-06 10:23:47 +0530

    inputing lists and concatenating them

---
+++ inputingliststruly.py
@@ -0,0 +1,13 @@
+# l1 = list(map(int, input("Enter list 1: ").split()))
+# l2 = list(map(int,input("Enter list 2: ").split()))
+
+l4=[]
+for i in range(5):
+    a = int(input("Enter the element: "))
+    l4.append(a**2)
+    
+print("New List: ", l4)
+
+
+# l3 = l1+l2
+# print("nEW list: ", l3)
