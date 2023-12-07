a88506e43bf6ea648288884c30e9b94a2042a3d8
Priyanshav <priyanshavkmr6@gmail.com>
2023-12-07 16:12:45 +0530

    modules

--- importingdefinedfn.py
+++ importingdefinedfn.py
@@ -1,7 +1,7 @@
 import definingmodule
 
-from definingmodule import add,multiply
+from definingmodule import *
 
 print("After adding a,b : ",add(20,40))
-# print(power(10,2))
+print(power(10,2))
 print("After multiplying a,b : ",multiply(10,60))
