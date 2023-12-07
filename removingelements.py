2a84b8b4a5b17d3a472237756802221033490a3a
Priyanshav <priyanshavkmr6@gmail.com>
2023-12-07 16:11:19 +0530

    removing repeated elements from a list

--- removingelements.py
+++ removingelements.py
@@ -1,8 +1,10 @@
 ls = list(map(int,input("Enter list: ").split()))
-l =[]
-
-for i in ls:
-    if i not in l:
-        l.append(i)
-        
+# l =[]
+s = set(ls)
+l = list(s)
 print(l)
+
+# for i in ls:
+#     if i not in l:
+#         l.append(i)
+# print(l)
