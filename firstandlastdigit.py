a8a2a634707aaf3d45798ac843b68a214e155d93
Priyanshav <priyanshavkmr6@gmail.com>
2023-12-07 10:25:46 +0530

    checking digit is even or odd

---
+++ firstandlastdigit.py
@@ -0,0 +1,13 @@
+n = input("Enter a number:")
+t = n
+
+# while(len(n) == 4):
+a = int(t[0])
+b = int(t[-1])
+if(a%2==0 and b%2==0):
+    print("True")
+elif(a%2!=0 and b%2!=0):
+    print("True")
+else:
+    print("False")
+    # break
